b48fa60330884052469216a9a1e8ccf1254382da
EugeneZakhno <zohan1234567@gmail.com>
2020-05-20 00:17:22 +0300

    first commit

---
+++ src/main/java/com/meanenglish/demoenglish/DemoenglishApplication.java
@@ -0,0 +1,22 @@
+package com.meanenglish.demoenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class DemoenglishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoenglishApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return  new RestTemplateBuilder().build();
+    }
+
+
+}
---
+++ src/test/java/com/meanenglish/demoenglish/DemoenglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meanenglish.demoenglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoenglishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
