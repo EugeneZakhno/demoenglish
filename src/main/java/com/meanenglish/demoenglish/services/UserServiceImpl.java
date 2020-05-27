a5d233c41e76b2d99f405578588981b8888ec906
EugeneZakhno <zohan1234567@gmail.com>
2020-05-27 22:56:19 +0300

    added @Service

--- src/main/java/com/meanenglish/demoenglish/services/UserServiceImpl.java
+++ src/main/java/com/meanenglish/demoenglish/services/UserServiceImpl.java
@@ -1,8 +1,14 @@
 package com.meanenglish.demoenglish.services;
 
 import com.meanenglish.demoenglish.model.User;
+import com.meanenglish.demoenglish.repositories.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
     @Override
     public User getUser() {
         return null;
